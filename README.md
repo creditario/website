befc7e92f9c0d9b42cabe199529b41f93340d1f6
Alejandro Zaizar <alejandro.zaizar@michelada.io>
2020-09-25 17:22:18 +0000

    New assets

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Home Page of Creditar.io
+
+## Deploy
+
+> yarn deploy
+
+Esto genera un folder `output/` en el cual van los assets staticos.
--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,3 +1,33 @@
-import "../styles/index.scss"
+import "../styles/index.scss";
+import { Application } from "stimulus";
+import { definitionsFromContext } from "stimulus/webpack-helpers";
 
-console.info("Bridgetown is loaded!")
+const application = Application.start();
+const context = require.context("./controllers", true, /.js$/);
+application.load(definitionsFromContext(context));
+
+let navbar = document.getElementById("navbar");
+let navButton = document.getElementById("navButton");
+let navMenu = document.getElementById("navMenu");
+let burger = document.getElementById("burger");
+let close = document.getElementById("close");
+
+navButton.addEventListener("click", function () {
+  if (window.scrollY == 0) {
+    navbar.classList.toggle("change");
+  }
+  navMenu.classList.toggle("hidden");
+  burger.classList.toggle("hidden");
+  close.classList.toggle("hidden");
+});
+
+window.addEventListener("scroll", function () {
+  if (
+    window.scrollY == 0 &&
+    document.getElementById("navbar").classList.contains("change")
+  ) {
+    navbar.classList.remove("change");
+  } else {
+    navbar.classList.add("change");
+  }
+});
