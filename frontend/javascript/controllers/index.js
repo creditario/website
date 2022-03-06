6c364bd6dec99321eb47c9f13b4b292a51aaee04
Mario Alberto Chávez <mario.chavez@gmail.com>
2022-03-05 19:23:58 -0600

    Menú para móvil

---
+++ frontend/javascript/controllers/index.js
@@ -0,0 +1,4 @@
+import { application } from "./application"
+
+import MenuController from "./menu_controller"
+application.register("menu", MenuController)
--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -3,4 +3,4 @@ import "index.css"
 // Import all JavaScript & CSS files from src/_components
 import components from "bridgetownComponents/**/*.{js,jsx,js.rb,css}"
 
-console.info("Bridgetown is loaded!")
+import "controllers"
